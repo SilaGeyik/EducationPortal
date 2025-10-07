1f601c95a01a4c3caaf8f4e840b00064954cd609
Sıla Geyik <silagyk06@gmail.com>
2025-10-07 20:47:43 +0300

    İlk commit – proje başlangıcı

--- EducationPortal.Web/Controllers/HomeController.cs
+++ EducationPortal.Web/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ namespace EducationPortal.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
